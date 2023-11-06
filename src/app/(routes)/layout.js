1321eaf3c2e27348e05533d1ee6046c29140a509
amerkhanfar <amer@oplus.ae>
2023-11-07 00:31:28 +0400

    added thank you message after submitting

--- src/app/(routes)/layout.js
+++ src/app/(routes)/layout.js
@@ -48,7 +48,8 @@ export default function RootLayout({ children }) {
         />
         <meta
           name='viewport'
-          content='width=device-width, initial-scale=1, maximum-scale=1'></meta>
+          content='width=device-width, initial-scale=1, maximum-scale=1'
+        />
         <link
           rel='apple-touch-icon'
           sizes='180x180'
